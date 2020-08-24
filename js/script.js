75bec467fd9da1e206c66789c8d744491ad6e79d
user <user@MacBook-Pro-user.local>
2020-08-24 21:45:35 +0300

    Timer

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
 
-    let tab = document.querySelectorAll('.info-header-tab'),
-        tabContent = document.querySelectorAll('.info-tabcontent'),
-        info = document.querySelector('.info-header');
+    function hideTabs(butt, cont, buttParent) {
+
+       let tab = document.querySelectorAll(butt),
+        tabContent = document.querySelectorAll(cont),
+        info = document.querySelector(buttParent);
    
         
       function hideTabContent(a) {
@@ -35,6 +37,72 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
       });
+    }
+
+    hideTabs('.info-header-tab', '.info-tabcontent', '.info-header');
+
+    
+    // Timer
+    
+    let deadline = '2020-08-25';
+
+    function getTimeRemaining(endtime) {
+      let t = Date.parse(endtime) - Date.parse(new Date()),
+          seconds = Math.floor(t / 1000 % 60),
+          minutes = Math.floor(t / 1000 / 60 % 60),
+          hours = Math.floor(t / 1000 / 60 / 60);
+          
+
+          return {
+            'total': t,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours
+          };
+    }
+
+    function getClock(id, endtime) {
+      let timer = document.getElementById(id),
+          seconds = timer.querySelector('.seconds'),
+          minutes = timer.querySelector('.minutes'),
+          hours = timer.querySelector('.hours'),
+          setTimer = setInterval(updateClock, 1000);
+
+          
+         
+      function updateClock() {
+          let t = getTimeRemaining(endtime);
+          
+          if (t.seconds < 10) {
+            seconds.textContent = '0' + t.seconds;
+          } else {
+            seconds.textContent = t.seconds;
+          }
+          
+          if (t.minutes < 10) {
+          minutes.textContent = '0' + t.minutes;
+          } else {
+            minutes.textContent =  t.minutes;
+          }
+
+
+          if (t.hours < 10) {
+            hours.textContent = '0' + t.hours;
+          } else {
+          hours.textContent = t.hours;
+          }
+          
+          if (t.total <= 0) {
+            clearInterval(setTimer);
+              seconds.textContent = '00';
+              minutes.textContent = '00';
+              hours.textContent = '00';
+          }
+ 
+      }
+    }
+
+      getClock('timer', deadline);
 });
 
 
